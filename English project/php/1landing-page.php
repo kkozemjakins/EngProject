3e8cf3a732de39d3abc27ac40380d435b6db8dcb
Kirils Kozemjakins <78017496+kkozemjakins@users.noreply.github.com>
2023-05-26 20:00:17 +0300

    Add files via upload

---
+++ English project/php/1landing-page.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/header-menu.css">
+    <link rel="stylesheet" href="../css/php/1landing-page.css">
+    <title>Home</title>
+</head>
+<body>
+
+<?php include 'templates/header-menu.html'; ?>
+
+<div class="container">
+
+    <div class="project-info">
+        <p class="title">How can we use digital tools to direct self-studying for our final English exam in secondary school?</p>
+
+        <p class="credits">English project by Marks Ansbergs, Arnis 
+        Borisevičs, Daniels Ērglis, Jānis Jēkabs
+        Menke, Vladimirs Serjogins, Asja
+        Zaharenko and Konstantīns Zarudnijs</p>
+    </div>
+
+    <div class="rmmt-logo">
+        <img class="main-logo-rmmt" src="../source/img/RMMT-logo.png" alt="rmmt-logo">
+    </div>
+
+</div>
+
+</body>
+</html>
